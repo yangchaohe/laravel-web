73dedc8359a629c2105ce054a4fa85a35f29d3c6
yangchaohe <manu2x@qq.com>
2021-08-22 12:48:13 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,18 +6,12 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a href="/" class="navbar-brand">Web App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a href="/help" class="nav-link">Help</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-        </ul>
-      </div>
-    </nav>
-
+    @include('layouts._header')
     <div class="container">
-      @yield('content')
+      <div class="offset-md-0 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
 
   </body>

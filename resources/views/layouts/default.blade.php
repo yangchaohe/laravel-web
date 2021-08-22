af3d1c9bd44c43d4e2393690734b282073a51a5d
yangchaohe <manu2x@qq.com>
2021-08-22 10:52:32 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,23 @@
 <html>
   <head>
     <title>@yield('title') - 余光微博</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a href="/" class="navbar-brand">Web App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a href="/help" class="nav-link">Help</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
+
   </body>
 </html>

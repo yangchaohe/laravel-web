fc67604d80d27d1b284e3f70ce197aa6aa03f63a
yangchaohe <manu2x@qq.com>
2021-08-31 00:08:16 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Manu';
         $user->email = 'manu2x@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
